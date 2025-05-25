56243c3e63b51586344a440b8c19aa0660f322e9
antonio-git25 <anton.lutsenko1990@yandex.ru>
2025-05-25 23:26:19 +0300

    Add files via upload

---
+++ pages/finish_page.py
@@ -0,0 +1,26 @@
+from datetime import time
+import datetime
+import time #for time sleep option
+
+import allure
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from base.base_class import Base
+
+
+class Finish_page(Base):
+
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.driver = driver
+
+
+
+    #Methods
+    def finish(self):
+        with allure.step("finish"):
+            self.get_current_url()
+            self.assert_url("https://www.saucedemo.com/checkout-complete.html")
+            self.get_screenshot()
